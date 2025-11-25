aaaacbe9c14e824fd38c4bd41561113f423a875e
ANUBHAV PANCHAL <panchalganubhav@gmail.com>
2025-11-25 17:05:18 +0530

    modification

--- demo.py
+++ demo.py
@@ -1,16 +1,3 @@
-# from us_visa.logger import logging
-# from us_visa.exception import USvisaException
-# import sys
-
-# logging.info("Welcome to our custom log")
-# try:
-#     a = 1 / "10"
-# except Exception as e:
-#     raise USvisaException(e, sys) from e
-
-
-
-
 from us_visa.pipeline.training_pipeline import TrainPipeline
 
 
--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -5,7 +5,7 @@ DATABASE_NAME = "US_VISA"
 
 COLLECTION_NAME = "visa_data"
 
-MONGODB_URL_KEY = "mongodb+srv://panchalganubhav_db_user:w9oWofSeo2WlYhh8@cluster0.i1kwfy3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0MONGODB_URL"
+MONGODB_URL_KEY = "mongodb+srv://panchalganubhav_db_user:<db_password>@cluster0.i1kwfy3.mongodb.net/?appName=Cluster0"
 
 PIPELINE_NAME: str = "usvisa"
 ARTIFACT_DIR: str = "artifact"
